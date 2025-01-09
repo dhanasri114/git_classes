50e9760e20b59ac41d57f4ee72ca7a5a116fa4e9
dhanu <dhanasrisai11@gmail.com>
2025-01-09 15:25:29 +0530

    after chane

---
+++ demo.js
@@ -0,0 +1,3 @@
+// hi all welcome to the java -script 
+// class today we are leaen to java script 
+//   please join the class in time
---
+++ demo1.java
@@ -0,0 +1,7 @@
+hi this is java class 
+  java is most popular lang
+    we can learn java 
+       if we use java create application
+         java is best programmig languge
+            
+          
